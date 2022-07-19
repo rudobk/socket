3f5f2d0c8ae07de8111fe2e90ab6ac9d63a22736
rudobk <102497116+rudobk@users.noreply.github.com>
2022-07-19 15:55:18 +0700

    Init

---
+++ Client.c
@@ -0,0 +1,141 @@
+#include<stdio.h>
+#include<sys/socket.h>
+#include<string.h>
+#include<stdlib.h>
+#include<arpa/inet.h>
+#include<pthread.h>
+#include<unistd.h>
+#include<signal.h>
+#define MAX 1024
+#define MAX_NAME 10
+int running = 1;
+int listen_fd;
+char name[MAX_NAME];
+char *prvCMD = "PRV*";
+typedef struct {
+    char name[10];
+    char msg[1024];
+    enum{
+        PUBMSG,
+        PRIMSG,
+        INIT
+    } ACTION;
+    char pname[10];
+} MSG;
+void catch_CTRL_C(int sig){
+    running = 0;
+}
+void str_trim_lf (char* arr, int length) {
+  int i;
+  for (i = 0; i < length; i++) {
+    if (arr[i] == '\n') {
+      arr[i] = '\0';
+      break;
+    }
+  }
+}
+void *send_msg_handler(void *args){
+    char buffer[MAX] = {0};
+    MSG sMSG = {0};
+    char *pname;
+    char *msg;
+    strcpy(sMSG.name, name);
+    while(1){
+        fflush(stdout);
+        fflush(stdin);
+        fgets(buffer, MAX, stdin);
+        str_trim_lf(buffer, MAX);
+        if(strstr(buffer, prvCMD) != NULL){
+            pname = strtok(buffer, "*");
+            pname = strtok(NULL,"*");
+            msg = strtok(NULL,"*");
+            if(pname == NULL || msg == NULL){
+                printf("Syntax error\n");
+                continue;
+            }
+            sMSG.ACTION = PRIMSG;
+            strcpy(sMSG.name, name);
+            strcpy(sMSG.pname, pname);
+            strcpy(sMSG.msg, msg);
+            send(listen_fd, (void*)&sMSG, sizeof(sMSG), 0);
+            memset(&sMSG, 0, sizeof(sMSG));  
+            continue;
+        }
+        strcpy(sMSG.msg, buffer);
+        send(listen_fd, (void*)&sMSG, sizeof(sMSG), 0);
+        memset(&sMSG, 0, sizeof(sMSG));  
+    }
+}
+void *recv_msg_handler(void *args){
+    MSG sMSG = {0};
+    while(1){
+        int receive = recv(listen_fd, (void*)&sMSG, sizeof(sMSG), 0);
+        if(receive <= 0){
+            exit(1);
+        }else{
+            if(sMSG.ACTION == PRIMSG){
+                fflush(stdout);
+                printf("(PRIVATE) %s: %s\n", sMSG.name, sMSG.msg);
+                fflush(stdout);
+                memset(&sMSG, 0, sizeof(sMSG));
+                continue;
+            }
+            printf("%s: %s\n", sMSG.name, sMSG.msg);
+            fflush(stdout);
+        }
+        memset(&sMSG, 0, sizeof(sMSG));
+    }
+}
+int main(int argc, char *argv[]){
+    MSG nMSG = {0};
+    if(argc != 2){
+        printf("Enter port number correct");
+        exit(1);
+    }
+
+    char *ip = "127.0.0.1";
+    int port = atoi(argv[1]);
+
+    signal(SIGINT, catch_CTRL_C);
+
+    struct sockaddr_in serverAddress;
+
+    listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    serverAddress.sin_family = AF_INET;
+	serverAddress.sin_addr.s_addr = inet_addr(ip);
+	serverAddress.sin_port = htons(port);
+    printf("Nhập tên: ");
+    fgets(name, MAX_NAME, stdin);
+    str_trim_lf(name, MAX_NAME);
+    if (connect(listen_fd, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) != 0) {
+        printf("Connection with the server failed...\n");
+        exit(0);
+    } else{
+        printf("Connected to Server\n");
+    }
+    strcpy(nMSG.name, name);
+    nMSG.ACTION = INIT;
+    send(listen_fd, (void*)&nMSG, sizeof(nMSG), 0);
+    printf("====Welcome to chat room =====\n");
+    printf("Syntax: PRV*NAME*MESSAGE to send private message\n");
+    pthread_t send_msg_thread;
+    if(pthread_create(&send_msg_thread, NULL, send_msg_handler, NULL) != 0){
+        printf("ERROR create send msg\n");
+        exit(1);
+    }
+    pthread_detach(send_msg_thread);
+    pthread_t recv_msg_thread;
+    if(pthread_create(&recv_msg_thread, NULL, recv_msg_handler, NULL) != 0){
+        printf("ERROR create send msg\n");
+        exit(1);
+    }
+    pthread_detach(recv_msg_thread);
+    while (1){
+		if(running == 0){
+			printf("\nBye\n");
+			break;
+        }
+	}
+	close(listen_fd);
+
+}
---
+++ Server.c
@@ -0,0 +1,158 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/time.h>
+#include <pthread.h>
+#define MAX_CLIENTS 30
+typedef struct{
+    int socket_client;
+    char name[10];
+} CLIENT;
+typedef struct {
+    char name[10];
+    char msg[1024];
+    enum{
+        PUBMSG,
+        PRIMSG,
+        INIT
+    } ACTION;
+    char pname[10];
+} MSG;
+MSG tempmsg = { 0 };
+CLIENT client[MAX_CLIENTS] = {0};
+int master_socket, addrlen, new_client;
+void *HandlerThread(void *args)
+{
+    struct sockaddr_in address;
+    int max_fd, activity, read_val;
+    int fd;
+    fd_set readfds;
+    struct timeval tv;
+    while(1){
+        max_fd = 0;
+        FD_ZERO(&readfds);
+
+        for(int i=0; i<MAX_CLIENTS; i++){
+            fd = client[i].socket_client;
+
+            if(fd > 0){
+                FD_SET(fd, &readfds);
+            }
+            if(fd > max_fd)
+                max_fd = fd;
+        }
+
+        if(max_fd == 0) continue;
+        tv.tv_sec = 3;
+        activity = select(max_fd + 1, &readfds, NULL, NULL, &tv);
+        if ((activity < 0) && (errno!=EINTR))
+		{
+			printf("Select error");
+            continue;
+		}
+        for(int i = 0; i< MAX_CLIENTS; i++)
+        {
+            fd = client[i].socket_client;
+            if(FD_ISSET(fd, &readfds))
+            {
+                memset(&tempmsg, 0 , sizeof(tempmsg));
+                if((read_val = read(fd, (void *)&tempmsg, sizeof(tempmsg))) == 0)
+                {
+                    /* Client disconnected */
+                    getpeername(fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+                    printf("Client disconnected, ip %s, port %d \n", inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+                    close( fd );
+                    client[i].socket_client = 0;
+                    memset(&client[i].name, 0 , sizeof(client[i].name));
+                }else{
+                    if(tempmsg.ACTION == INIT){
+                        strcpy(client[i].name, tempmsg.name);
+                        break;
+                    }
+                    if(tempmsg.ACTION == PRIMSG){
+                            for(int j = 0; j< MAX_CLIENTS; j++)
+                        {
+                            if(strcmp(client[j].name, tempmsg.pname) == 0)
+                            {
+                                send(client[j].socket_client, (void*)&tempmsg, sizeof(tempmsg), 0);
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                    for(int j = 0; j< MAX_CLIENTS; j++)
+                    {
+                        if(client[j].socket_client != fd)
+                        {
+                            send(client[j].socket_client, (void*)&tempmsg, sizeof(tempmsg), 0);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+int main(int argc, char *argv[])
+{
+    if(argc != 2){
+        printf("Enter port number correct");
+        exit(1);
+    }
+    int port = atoi(argv[1]);
+
+    fd_set readfds;
+    struct sockaddr_in address;
+    pthread_t clientHT;
+    for(int i = 0; i < MAX_CLIENTS; i++)
+    {
+        client[i].socket_client = 0;
+    }
+    if((master_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        printf("Failed to create Master Socket");
+        exit(1);
+    }
+    address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(port);
+    addrlen = sizeof(address);
+    if (bind(master_socket, (struct sockaddr *)&address, sizeof(address))<0)
+	{
+		printf("Faild to Bind Socket");
+        exit(1);
+	}
+
+    printf("Listener on port %d \n", port);
+
+    if(listen(master_socket, 10) < 0)
+    {
+        printf("Failed to listen on port %d\n", port);
+        exit(1);
+    }
+    pthread_create(&clientHT, NULL, HandlerThread, NULL);
+    pthread_detach(clientHT);
+    printf("Start waiting for connections\n");
+
+    while(1){
+        if((new_client = accept(master_socket, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0)
+        {
+            printf("Failed to accept client");
+            exit(1);
+        }
+        printf("New connection , socket fd is %d , ip is : %s , port : %d \n" , new_client , inet_ntoa(address.sin_addr) , ntohs(address.sin_port));
+
+        for(int i = 0; i < MAX_CLIENTS; i++)
+        {
+            if(client[i].socket_client == 0){
+                client[i].socket_client = new_client;
+                break;
+            }
+        }
+    }
+}
